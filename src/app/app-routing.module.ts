490c7749b06e5417f2e61a51b3c7cd21c7271df8
Libb521 <eotieno39@yahoo.com>
2020-03-28 01:06:33 +0300

    imported the created components into the app-routing module and created routing paths for each

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HoodComponent } from './hood/hood.component';
+import { AuthorityComponent } from './authority/authority.component';
+import { BusinessComponent } from './business/business.component';
+import { HealthComponent } from './health/health.component';
+import { BlogsComponent } from './blogs/blogs.component';
+import { OutComponent } from './out/out.component';
+import { HomeComponent } from './home/home.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'hood', component: HoodComponent },
+  { path: 'authority', component: AuthorityComponent },
+  { path: 'business', component: BusinessComponent },
+  { path: 'health', component: HealthComponent },
+  { path: 'blogs', component: BlogsComponent },
+  { path: 'log-out', component: OutComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
