12ae2c39c03883c2f3c9d009777222c8c0fe413a
Libb521 <eotieno39@yahoo.com>
2020-03-28 01:02:32 +0300

    Imported the created components into the module.ts file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
+import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthorityComponent } from './authority/authority.component';
@@ -26,7 +32,15 @@ import { OutComponent } from './out/out.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    MatCardModule,
+    MatSliderModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatIconModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
